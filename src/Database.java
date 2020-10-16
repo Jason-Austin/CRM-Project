a036f0beff9a429291d3da446761f277939eeb1e
Austin Varnon <austinvarnon@gmail.com>
2020-10-16 15:07:52 -0500

    FEAT: added "addContact" functionality

--- src/Database.java
+++ src/Database.java
@@ -1,20 +1,24 @@
 import files.FileIO;
 
+
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Database {
+
+
     public static void main(String[] args) throws IOException {
         String directoryName = "Contacts";
         String fileName = "Contacts.txt";
 
-        try {
-            Path dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
+        Path dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
+//        ArrayList<String> contactList = new ArrayList<>();
+        List<String> contactList = Arrays.asList();
+        DatabaseModifier.addContact(dataFilePath, "Austin", "8303914229");
 
 
-        } catch (IOException ex) {
-            System.out.println("Cannot Create the file");
-            ex.printStackTrace();
-        }
     }
 }
