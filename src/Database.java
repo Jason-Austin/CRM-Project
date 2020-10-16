c0342cc8be76f1d160c045272ec6216d04d8a918
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-16 15:53:15 -0600

    added intitial sout

--- src/Database.java
+++ src/Database.java
@@ -16,6 +16,14 @@ public class Database {
 
         Path dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
 
+        System.out.println("Welcome to Contact List");
+        System.out.println("1-View Contacts");
+        System.out.println("2-Add new Contact");
+        System.out.println("3-Search contact by name");
+        System.out.println("4-Delete existing contact");
+        System.out.println("0-Exit");
+
+
         Input input = new Input();
 
         System.out.println("Would you like to enter a New contct? Y/N");
