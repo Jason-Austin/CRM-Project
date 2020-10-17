04158bf8a31b33950aa53a25075a244b168ad2b8
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-17 10:32:47 -0600

    added view all contacts list

--- src/Database.java
+++ src/Database.java
@@ -16,6 +16,8 @@ public class Database {
 
         Path dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
 
+        Input input = new Input();
+
         System.out.println("Welcome to Contact List");
         System.out.println("1-View Contacts");
         System.out.println("2-Add new Contact");
@@ -23,9 +25,16 @@ public class Database {
         System.out.println("4-Delete existing contact");
         System.out.println("0-Exit");
 
+        //VIEW ALL CONTACTS
+        System.out.println("Would you like to view all contacts? Y/N");
+        String userConfirm = input.getString();
 
-        Input input = new Input();
+        if (userConfirm.equalsIgnoreCase("y")) {
 
+            FileIO.printFileContents(dataFilePath);
+        }
+
+//         ADD NEW CONTACT
         System.out.println("Would you like to enter a New contact? Y/N");
         String userInput = input.getString();
 
